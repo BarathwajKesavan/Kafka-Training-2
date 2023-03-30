f80580b44fbce31732232c86120b37f123f99788
Barathwaj Kesavan <barathwaj1994@gmail.com>
2023-03-30 14:56:48 +0530

    Kafka Consumer

---
+++ src/main/java/com/kafka/ConsumerDemo.java
@@ -0,0 +1,43 @@
+package com.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org. apache.kafka.clients.consumer.ConsumerRecords;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemo {
+
+    public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
+        String groupId ="my-fourth-application";
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupID = "My Fourth Application";
+        String topic = "replication_topic";
+        Properties property=new Properties();
+        property.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
+        property.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        property.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+        property.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        property.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
+        KafkaConsumer<String,String> consumer= new KafkaConsumer<String, String>(property);
+        consumer.subscribe(Arrays.asList(topic));
+
+        while(true){
+            ConsumerRecords<String,String> records = consumer.poll(Duration.ofMillis(100));
+            for(ConsumerRecord<String,String> record: records){
+                System.out.println(record.key()+record.value()+record.offset()+record.topic()+record.partition());
+            }
+        }
+
+    }
+}
--- src/main/java/com/kafka/ProducerDemo.java
+++ src/main/java/com/kafka/ProducerDemo.java
@@ -1,9 +1,8 @@
 package com.kafka;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
-import  org.apache.kafka.clients.producer.KafkaProducer;
+
 import java.util.Properties;
 
 public class ProducerDemo {
@@ -15,10 +14,25 @@ public class ProducerDemo {
         property.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
 
        KafkaProducer<String,String> producer = new KafkaProducer<String,String>(property);
+    //record using keys
+       ProducerRecord<String,String> record = new ProducerRecord<>("replication_topic","1","Helloworld1");
+        ProducerRecord<String,String> record2 = new ProducerRecord<>("replication_topic","2","Helloworld2");
+        ProducerRecord<String,String> record3 = new ProducerRecord<>("replication_topic","3","Helloworld3");
 
-       ProducerRecord<String,String> record = new ProducerRecord<>("replication_topic","Barath","Barath:Helloworld");
+    //record without keys
+        ProducerRecord<String,String> record1 = new ProducerRecord<>("replication_topic","Barath:Helloworld");
 
        producer.send(record);
+        producer.send(record1);
+        producer.send(record2);
+        producer.send(record3, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                System.out.println("Offset "+ recordMetadata.offset()+"Topic "+recordMetadata.topic() );
+            }
+        });
+
+        //use flush when you need to push the message
        producer.flush();
        producer.close();
 
